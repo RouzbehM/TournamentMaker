74f38c9528b7ddf6d7790c300737b13ccc8d5037
RouzbehM <rmaji037@uottawa.ca>
2015-12-02 17:52:08 -0500

    added help activity, added help option

--- TournamentMaker/app/src/main/java/geeks/tournamentmaker/MainActivity.java
+++ TournamentMaker/app/src/main/java/geeks/tournamentmaker/MainActivity.java
@@ -88,8 +88,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(id == R.id.action_settings){
+            Intent intent = new Intent(this, ViewHelp.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
