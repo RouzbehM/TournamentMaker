b6d3c11f76548fb98e33567af90a045c5cbaa324
RouzbehM <rmaji037@uottawa.ca>
2015-12-03 11:32:49 -0500

    completed help page

--- TournamentMaker/app/src/main/java/geeks/tournamentmaker/help/ViewStandings.java
+++ TournamentMaker/app/src/main/java/geeks/tournamentmaker/help/ViewStandings.java
@@ -2,13 +2,21 @@ package geeks.tournamentmaker.help;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import geeks.tournamentmaker.R;
 
 public class ViewStandings extends ActionBarActivity {
 
+    private ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_view_standings2);
+
+
+
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        mImageView.setImageResource(R.drawable.view_standings);
     }
 }
